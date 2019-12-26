9f11acc556e887a11a4d26ab3d1f8cb2ccb48566
nikinchitrakar <nikinchitrakar@gmail.com>
2019-12-26 20:21:43 +0545

    Robofriends: React Project

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'John',
+        username: 'Cena',
+        email: 'john@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Bat',
+        username: 'Man',
+        email: 'batman@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Super',
+        username: 'Woman',
+        email: 'superwoman@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Bruce',
+        username: 'Wayne',
+        email: 'brucewayne@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Peter',
+        username: 'Parker',
+        email: 'peterparker@gmail.com'
+    },
+]
