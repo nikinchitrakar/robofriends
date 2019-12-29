44a4610482c8fa243f20ef8436f5da9b1a4122e7
nikinchitrakar <nikinchitrakar@gmail.com>
2019-12-29 19:39:29 +0545

    Updates

---
+++ precache-manifest.178d68eca599c143cf0b9d230596aae3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce2f4e2924b3532621bc86e2c5b61ba",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4af4fc60f8e35e58d444",
+    "url": "/robofriends/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/robofriends/static/js/2.8d7c7b1f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.8d7c7b1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4af4fc60f8e35e58d444",
+    "url": "/robofriends/static/js/main.faac0fcb.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
