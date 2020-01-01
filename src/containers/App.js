dc5e1eda522cf5139286bb1d46022e9ba6f38082
nikinchitrakar <nikinchitrakar@gmail.com>
2020-01-01 17:28:40 +0545

    redux thunk addition

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,51 +4,43 @@ import CardList from '../components/CardList';
 import Searchbox from '../components/Searchbox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   }
 }
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
-
+  
   componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({robots: users}));
+    this.props.onRequestRobots();
   }
-
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value})
-  }
-
+  
   render(){
-    const { robots, searchfield } = this.state;
+    const { searchfield, robots, isPending, onSearchChange } = this.props;
+    console.log("robots::::",  robots);
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    if (!robots.length){
+    if (isPending){
       return <h1>Loading</h1>
     }else{
       return(
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
-          <Searchbox searchChange = {this.onSearchChange}/>
+          <Searchbox searchChange = {onSearchChange}/>
           <Scroll>
             <ErrorBoundry>
               <CardList robots={filteredRobots}/>
