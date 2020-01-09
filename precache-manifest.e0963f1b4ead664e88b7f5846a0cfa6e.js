a340ce7940ec40bb65b4bc352ab393136a607f29
nikinchitrakar <nikinchitrakar@gmail.com>
2020-01-09 16:13:51 +0545

    Updates

---
+++ precache-manifest.e0963f1b4ead664e88b7f5846a0cfa6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add5d6f066cd501e45f0e8971ee2c42a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "123e006c2faf5065fd39",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ed5e6158701b79f15350",
+    "url": "/robofriends/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "123e006c2faf5065fd39",
+    "url": "/robofriends/static/js/2.665c8f6b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/robofriends/static/js/2.665c8f6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed5e6158701b79f15350",
+    "url": "/robofriends/static/js/main.742d4681.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
--- src/components/Searchbox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Searchbox = ({searchChange}) => {
-    return (
-        <input type="search" placeholder="Enter Robo name" onChange={searchChange}/>
-    )
-}
-
-export default Searchbox;
